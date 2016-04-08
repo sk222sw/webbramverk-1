407ccbf911d954bc764c309c857a454c2cccd22b
Sonny Kjellberg <sk222sw@student.lnu.se>
2016-04-08 17:25:55 +0000

    users can create apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,10 +9,22 @@ class AppsController < ApplicationController
     end
 
     def create
-        @app = App.new(app_params)
+        @app = current_user.apps.build(apps_params)
+        if @app.save
+            flash[:notice] = "App created - woop woop!"
+            redirect_to @current_user
+        else
+            flash[:notice] = "Woops, something wen't wrong :/"
+        end
     end
     
     def new
         @app = App.new
+    end
+    
+    private
+    
+    def apps_params
+        params.require(:app).permit(:name)
     end
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,5 @@
 class App < ActiveRecord::Base
     belongs_to :user
+    validates :user_id, presence: true
+    validates :name, presence: true, length: { minimum: 2 }
 end
